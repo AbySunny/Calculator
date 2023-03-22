0ef620ecc495f44ab107184cb3c9ab783237ceb6
Aby Sunny <abysunny2001@gmail.com>
2023-03-22 12:04:46 +0530

    Calculator

---
+++ index.js
@@ -0,0 +1,14 @@
+function displaydata(data){
+    result.value+=data
+}
+
+function allclear(){
+    result.value=""
+}
+function answer(){
+    result.value=eval(result.value)
+}
+function backspace(){
+    data=result.value
+    result.value=data.slice(0,-1)
+}
